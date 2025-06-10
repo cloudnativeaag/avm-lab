505abc507ad9c599c65a2e34b27193b4ac5d64f2
cloudnativeaag <saikiranitday30@gmail.com>
2025-06-10 19:20:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+# Create a random string to make resource names unique
 resource "random_string" "unique_name" {
-  length  = 3
-  special = false
-  upper   = false
-  numeric = false
+  length  = 3          # Length of the random string (3 characters)
+  special = false      # Don't include special characters like !@#
+  upper   = false      # Use only lowercase letters, no uppercase
+  numeric = false      # Don't include numbers, only letters
 }
+
+# Example usage:
+# This will generate a random string like "abc" or "xyz"
+# You can then add this to resource names to avoid naming conflicts
+# For example: "myapp-prod-eastus-abc"
+
 
 module "resource_group" {
   source   = "Azure/avm-res-resources-resourcegroup/azurerm"
